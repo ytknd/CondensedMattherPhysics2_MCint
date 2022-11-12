64e075b7cd725d2541323d1b58b0b9d751c3435a
Y.Kondo <yutknd@gmail.com>
2022-11-12 13:51:42 +0900

    modified:   mc_integral_2d_disk.cpp

--- mc_integral_2d_disk.cpp
+++ mc_integral_2d_disk.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-#include <vector>
 #include <fstream>
+#include <vector>
 #include <random>
-#define ATTEMPTS 1000000
-#define INTEGRAL_INTERVAL_MIN 0.0
-#define INTEGRAL_INTERVAL_MAX 1.0
+#define ATTEMPTS 100
+#define INTERVAL_MIN 0.0
+#define INTERVAL_MAX 1.0
 
 int main () {
     double sampled_plots, integrate_surface, radius =1.0;
@@ -12,7 +12,7 @@ int main () {
 
     std::random_device seed_gen;
     std::mt19937 engine (seed_gen());
-    std::uniform_real_distribution<> rand (INTEGRAL_INTERVAL_MIN, INTEGRAL_INTERVAL_MAX);
+    std::uniform_real_distribution<> rand (INTERVAL_MIN, INTERVAL_MAX);
 
     for (long int i = 0; i < ATTEMPTS; i++) {
 
@@ -27,4 +27,14 @@ int main () {
     integrate_surface = sampled_plots / ATTEMPTS * 4.0;
 
     std::cout << integrate_surface << std::endl;
+
+    std::ofstream ofs ("result_2d_disk.txt");
+    if (!ofs) {
+        std::cerr << "Fail to file open!" << std::endl;
+        std::exit(1);
+    }
+
+    for (long int i = 0; i < ATTEMPTS; i++) {
+        ofs << x.at(i) << "\t" << y.at(i) << std::endl;
+    }
 }
