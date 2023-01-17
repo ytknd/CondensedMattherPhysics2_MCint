e519ab2b48ce5d91557098bc423ba093c6a068f2
Y.Kondo <yutknd@gmail.com>
2023-01-17 19:44:31 +0900

    modified:   cpp/mc_integral_1_1.cpp

--- cpp/mc_integral_1_1.cpp
+++ cpp/mc_integral_1_1.cpp
@@ -35,15 +35,19 @@ int main () {
     gp = popen("gnuplot", "w");
     fprintf(gp, "set terminal png\n");
     fprintf(gp, "set terminal png size %d,%d\n", 600, 600);
+    fprintf(gp, "set output \"mc_integral_1_1.png\"\n");
     fprintf(gp, "set xrange[%f:%f]\n", INTERVAL_MIN, INTERVAL_MAX);
     fprintf(gp, "set yrange[%f:%f]\n", INTERVAL_MIN, INTERVAL_MAX);
     fprintf(gp, "set xlabel \"X\"\n");
     fprintf(gp, "set ylabel \"Y\"\n");
 
-    fprintf(gp, "plot x * x lc rgb \"red\", x * x * x lc rgb \"red\", \"-\" with points pt 0 lc rgb \"orange\"\n");
+    fprintf(gp, "plot x * x lc rgb \"red\", x * x * x lc rgb \"red\", '-' with points pt 0 lc rgb \"orange\"\n");
     for (int i = 0; i < x_plot.size(); i++) {
         fprintf(gp, "%f\t%f\n", x_plot.at(i), y_plot.at(i));
     }
     fprintf(gp, "e\n");
+
+    fflush(gp);
+    pclose(gp);
 }
 
