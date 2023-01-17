e2ba69b5584a60467d003e2e27d2302599edfb4f
Y.Kondo <yutknd@gmail.com>
2023-01-17 20:06:17 +0900

    modified:   cpp/mc_integral_1_3.cpp 	new file:   cpp/mc_integral_1_4.cpp 	modified:   cpp/mc_integral_2d_disk.cpp 	modified:   cpp/mc_integral_3d_sphere.cpp

--- cpp/mc_integral_1_3.cpp
+++ cpp/mc_integral_1_3.cpp
@@ -7,8 +7,9 @@
 
 int main () {
     int attempts = 1000000;
-    double x, y, z, sampled_plots, integrate_volume, radius =1.0;
+    double x, y, z, sampled_plots, integrate_volume, radius = 1.0;
     std::vector<double> x_plot, y_plot, z_plot;
+    FILE *gp;
 
     std::random_device seed_gen;
     std::mt19937 engine (seed_gen());
@@ -32,14 +33,24 @@ int main () {
 
     std::cout << integrate_volume << std::endl;
 
-    std::ofstream ofs ("mc_integral_1_3.txt");
-    if (!ofs) {
-        std::cerr <<"Fail to file open!" <<std::endl;
-        std::exit(1);
-    }
+    gp = popen("gnuplot", "w");
+    fprintf(gp, "set terminal png\n");
+    fprintf(gp, "set terminal png size %d,%d\n", 600, 600);
+    fprintf(gp, "set output \"mc_integral_1_3.png\"\n");
+    fprintf(gp, "set xrange[%f:%f]\n", INTERVAL_MIN, INTERVAL_MAX);
+    fprintf(gp, "set yrange[%f:%f]\n", INTERVAL_MIN, INTERVAL_MAX);
+    fprintf(gp, "set zrange[%f:%f]\n", INTERVAL_MIN, INTERVAL_MAX);
+    fprintf(gp, "set xlabel \"X\"\n");
+    fprintf(gp, "set ylabel \"Y\"\n");
+    fprintf(gp, "set zlabel \"Z\"\n");
 
-    for (long int i = 0; i < x_plot.size(); i++) {
-        ofs << x_plot.at(i) << "\t" << y_plot.at(i) << "\t" << z_plot.at(i) << std::endl;
+    fprintf(gp, "splot 1-x-y lc rgb \"red\", '-' with points pt 0 lc rgb \"orange\"\n");
+    for (int i = 0; i < x_plot.size(); i++) {
+        fprintf(gp, "%f\t%f\t%f\n", x_plot.at(i), y_plot.at(i), z_plot.at(i));
     }
+    fprintf(gp, "e\n");
+
+    fflush(gp);
+    pclose(gp);
 
 }
